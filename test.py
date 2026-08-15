31597fa567082432b67e539807b73243aaa28dc5
Vojtech <lickovojtech@gmail.com>
2026-08-15 10:06:59 +0200

    Experiment: nova metoda

--- test.py
+++ test.py
@@ -2,3 +2,4 @@ print('Ahoj Git')
 print('Toto je moj druhy riadok')
 print('ok, možeme pokračovať ďalej. kedy sa dostaneme aj na git hub?')
 # uprava na githube
+print('Toto je experimentalna zmena')
